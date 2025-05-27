16cc4cdda1772ce6732842308bf8698acac3cba9
VictorProcha <victorhugoprocha@hotmail.com>
2025-05-27 11:05:52 -0300

    eliminando scripts

--- assents/intro-js/plugins.js
+++ assents/intro-js/plugins.js
@@ -10656,22 +10656,7 @@ if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requir
         var b = a.offsetWidth - a.clientWidth;
         return this.$body[0].removeChild(a), b
     };
-    var d = a.fn.modal;
-    a.fn.modal = b, a.fn.modal.Constructor = c, a.fn.modal.noConflict = function() {
-        return a.fn.modal = d, this
-    }, a(document).on("click.bs.modal.data-api", '[data-toggle="modal"]', function(c) {
-        var d = a(this),
-            e = d.attr("href"),
-            f = a(d.attr("data-target") || e && e.replace(/.*(?=#[^\s]+$)/, "")),
-            g = f.data("bs.modal") ? "toggle" : a.extend({
-                remote: !/#/.test(e) && e
-            }, f.data(), d.data());
-        d.is("a") && c.preventDefault(), f.one("show.bs.modal", function(a) {
-            a.isDefaultPrevented() || f.one("hidden.bs.modal", function() {
-                d.is(":visible") && d.trigger("focus")
-            })
-        }), b.call(f, g, this)
-    })
+    
     "use strict";
 
     function b(b) {
